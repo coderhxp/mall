bee0246bebe9c6d71f2a066f59f1156032815b9f
coderhxp <3300505519@qq.com>
2021-01-05 00:04:28 +0800

    update

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tab-control">
+    <div :key="index" v-for="(item, index) in titles" 
+          class="tab-control-item"
+          :class="{active: index === currentIndex}"
+          @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabControl",
+    props: {
+      titles: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        currentIndex: 0
+      }
+    },
+    methods: {
+      itemClick(index) {
+        this.currentIndex = index;
+      }
+    }
+  }
+</script>
+
+<style>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    background-color: #fff;
+  }
+  .tab-control-item {
+    flex: 1;
+    height: 40xp;
+    line-height: 40px;
+  }
+  .active span {
+    padding: 5px;
+    border-bottom: 3px solid var(--color-tint);
+    color: var(--color-tint);
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,43 +4,170 @@
     <nav-bar class="home-nav"><div slot="center">购物车</div></nav-bar> 
     <home-swiper :banners="banners" />
     <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" />
+
+    <ul>
+      <li>1列表</li>
+      <li>2列表</li>
+      <li>3列表</li>
+      <li>4列表</li>
+      <li>5列表</li>
+      <li>6列表</li>
+      <li>7列表</li>
+      <li>8列表</li>
+      <li>9列表</li>
+      <li>1列表</li>
+      <li>2列表</li>
+      <li>3列表</li>
+      <li>4列表</li>
+      <li>5列表</li>
+      <li>6列表</li>
+      <li>7列表</li>
+      <li>8列表</li>
+      <li>9列表</li>
+      <li>10列表</li>
+      <li>11列表</li>
+      <li>12列表</li>
+      <li>13列表</li>
+      <li>14列表</li>
+      <li>15列表</li>
+      <li>16列表</li>
+      <li>17列表</li>
+      <li>18列表</li>
+      <li>19列表</li>
+      <li>20列表</li>
+      <li>21列表</li>
+      <li>22列表</li>
+      <li>23列表</li>
+      <li>24列表</li>
+      <li>25列表</li>
+      <li>26列表</li>
+      <li>27列表</li>
+      <li>28列表</li>
+      <li>29列表</li>
+      <li>30列表</li>
+      <li>31列表</li>
+      <li>32列表</li>
+      <li>33列表</li>
+      <li>34列表</li>
+      <li>35列表</li>
+      <li>36列表</li>
+      <li>37列表</li>
+      <li>38列表</li>
+      <li>39列表</li>
+      <li>40列表</li>
+      <li>41列表</li>
+      <li>42列表</li>
+      <li>43列表</li>
+      <li>44列表</li>
+      <li>45列表</li>
+      <li>46列表</li>
+      <li>47列表</li>
+      <li>48列表</li>
+      <li>49列表</li>
+      <li>50列表</li>
+      <li>51列表</li>
+      <li>52列表</li>
+      <li>53列表</li>
+      <li>54列表</li>
+      <li>55列表</li>
+      <li>56列表</li>
+      <li>57列表</li>
+      <li>58列表</li>
+      <li>59列表</li>
+      <li>60列表</li>
+      <li>61列表</li>
+      <li>62列表</li>
+      <li>63列表</li>
+      <li>64列表</li>
+      <li>65列表</li>
+      <li>66列表</li>
+      <li>67列表</li>
+      <li>68列表</li>
+      <li>69列表</li>
+      <li>70列表</li>
+    </ul>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from 'components/content/tabControl/TabControl'
+
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
+  import FeatureView from './childComps/FeatureView'
 
-  import {getHomeMultidata} from 'network/home'
+  import { getHomeMultidata, getHomeGoods } from 'network/home'
 
   export default {
     name: "Home",
     components: {
       NavBar,
+      TabControl,
+
       HomeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView,
     },
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        }
       }
     },
-    // 
+    // 组件创建时请求的数据
     created() {
-      getHomeMultidata().then(res => {
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-        // console.log(res);
-      })
+      // 1.请求多个数据
+      this.getHomeMultidata()
+
+      // 2.请求商品数据 res是局部变量,函数执行完会消失
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods: {
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+          // console.log(res);
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1;
+        getHomeGoods(type, page).then(res => {
+          // console.log(res);
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+        })
+      }
     }
   }
 </script>
 
 <style>
+  #home {
+    padding-top: 44px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
